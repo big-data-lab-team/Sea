fe217149b155ee6393d884347108a3dcd73b355d
valhayot <valeriehayot@gmail.com>
2020-05-15 12:06:37 -0400

    added more bash internal tests

--- tests/setup.bash
+++ tests/setup.bash
@@ -6,4 +6,8 @@ echo a > a.txt # This file will be used in tests
 echo b > ${SOURCE}/file_in_source.txt # This file will be used in tests
 mkdir ${SOURCE}/subdir
 echo a > ${SOURCE}/subdir/file_in_subdir.txt
+mkdir ${SOURCE}/bin # where fake binaries will be located
+echo "#!/bin/bash" > ${SOURCE}/bin/hello.sh
+echo "echo \"hello\"" > ${SOURCE}/bin/hello.sh
+chmod +x ${SOURCE}/bin/hello.sh
 export LD_PRELOAD=${PWD}/passthrough.so
