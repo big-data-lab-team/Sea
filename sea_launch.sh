47989161da57b58c929e01e9fc19838a90be6a1b
valhayot <valeriehayot@gmail.com>
2020-06-26 00:36:32 -0400

    added basic launch script

---
+++ sea_launch.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -e
+
+conf_file=${SEA_HOME}/sea.ini
+
+n_sources="$(($(awk -F "=" '/n_sources/ {print $2}' ${conf_file} | tr -d ' ;') - 1))"
+
+for ((i=0;i<${n_sources};i++))
+do
+    source=($(cat ${conf_file} | grep "source_$i" | cut -d "=" -f 2 | tr -d ' ;'))
+    mkdir ${source} 
+done
+
+bash ${SEA_HOME}/sea_flusher.sh &
+pid=$!
+
+echo "Launching script"
+$@
+
+kill $pid
