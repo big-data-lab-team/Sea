7efb33604b51b076c4af6077c260f5b181bf819a
valhayot <valeriehayot@gmail.com>
2020-07-16 12:17:44 -0400

    flushing improvements and added multidisks to experiments

--- bin/sea_flusher.sh
+++ bin/sea_flusher.sh
@@ -11,8 +11,32 @@ conf_file=${SEA_HOME}/sea.ini
 flush_file=${SEA_HOME}/.sea_flushlist
 
 
+get_rp_rsync () {
+    tobe_flushed=$1
+    flushlist=""
+    for f in $tobe_flushed
+    do
+        for s in "${sources_arr[@]}"
+        do
+            if [[ "$f" == "${s}/"* ]]
+            then
+                file_out=$(echo "${f/$s/$base_source}")
+                echo "flushing and evicting $f to $file_out"
+
+                if [[ "$flushlist" != "" ]]
+                then
+                    flushlist+=" "
+                fi
+                flushlist+=$f
+            fi
+        done
+    done
+    rsync -a --no-owner --no-group --remove-source-files ${flushlist} ${base_source} || ( sleep 5 && rsync -a --no-owner --no-group --remove-source-files ${flushlist} ${base_source} ) || echo "files cannot be flushed"
+}
+
 flush () {
         re_flush=""
+        sources_str=""
         for s in "${sources_arr[@]}"
         do
             while IFS="" read -r re || [ -n "$re" ]
@@ -29,25 +53,21 @@ flush () {
 
         if [[ $re_flush != "" ]]
         then
-            found_files=$(find ${re_flush} -type f -follow || echo "")
+            found_files=""
+            for rgx in ${re_flush}
+            do
+                if [[ $found_files != "" ]]
+                then
+                    found_files+=" "
+                fi
+
+                found_files+=$(find ${rgx} -type f -follow || echo "")
+            done
             
             if [[ $found_files != "" ]]
             then
                 tobe_flushed=$(ls -tur ${found_files})
-
-                for f in $tobe_flushed
-                do
-                    for s in $sources_arr
-                    do
-                        if [[ "$f" == "${s}/"* ]]
-                        then
-                            file_out=$(echo "${f/$s/$base_source}")
-                            #echo "flushing and evicting $f to $file_out"
-                            rsync -a --no-owner --no-group --remove-source-files "$f" "$file_out" || ( sleep 5 && rsync -a --no-owner --no-group --remove-source-files "$f" "$file_out" )
-                            break
-                        fi
-                    done
-                done
+                get_rp_rsync "${tobe_flushed}"
             fi
         fi
 }
@@ -62,24 +82,34 @@ flush_process () {
     done
 }
 
+cleanup () {
+    echo "Cleaning up Sea mount"
+    time flush
+}
+
 
 get_sources () {
 
     # refers to the last index and not the total number of sources
-    n_sources="$(($(awk -F "=" '/n_sources/ {print $2}' ${SEA_HOME}/sea.ini | tr -d ' ;') - 1))"
+    n_lvls="$(($(awk -F "=" '/n_levels/ {print $2}' ${SEA_HOME}/sea.ini | tr -d ' ;') - 1))"
 
-    for ((i=0;i<${n_sources};i++))
+    echo $n_lvls
+    for ((i=0;i<${n_lvls};i++))
     do
-        sources_arr+=($(cat ${conf_file} | grep "source_$i" | cut -d "=" -f 2 | tr -d ' ;'))
-        echo source_$i ${sources_arr[$i]}
+        source_lvl=$(cat ${conf_file} | grep "^\s*source_$i" | cut -d "=" -f 2 | tr -d ' ;')
+        IFS=',' read -ra curr_sources <<< "$source_lvl"
+
+        sources_arr+=(${curr_sources[@]})
+
+        echo source_$i ${sources_arr[@]}
     done
 
-    base_source="$(cat ${conf_file} | grep "source_$i" | cut -d "=" -f 2 | tr -d ' ;')"
+    base_source=$(cat ${conf_file} | grep "^\s*source_$i" | cut -d "=" -f 2 | tr -d ' ;')
     echo base dir ${base_source}
 
 }
 
-trap 'flush' SIGINT SIGTERM EXIT
+trap 'cleanup' SIGINT SIGTERM EXIT
 get_sources
 flush_process
 
