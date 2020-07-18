2039d74fa8c752409077f555ddb6ddc65dbd3235
valhayot <valeriehayot@gmail.com>
2020-07-17 21:09:18 -0400

    WIP:changed rsync to mv in the flusher

--- bin/sea_flusher.sh
+++ bin/sea_flusher.sh
@@ -10,31 +10,62 @@ n_sources=0
 conf_file=${SEA_HOME}/sea.ini
 flush_file=${SEA_HOME}/.sea_flushlist
 
-
-get_rp_rsync () {
+get_rp_mv () {
     tobe_flushed=$1
+    OLDTIME=$2
     flushlist=""
     for f in $tobe_flushed
     do
-        for s in "${sources_arr[@]}"
-        do
-            if [[ "$f" == "${s}/"* ]]
-            then
-                file_out=$(echo "${f/$s/$base_source}")
-                echo "flushing and evicting $f to $file_out"
 
-                if [[ "$flushlist" != "" ]]
-                then
-                    flushlist+=" "
-                fi
-                flushlist+=$f
+        if [ ! -f $f ]
+        then
+            continue
+        fi
+        # code adapted from here https://stackoverflow.com/a/28341234
+        # Get current and file times
+        CURTIME=$(date +%s)
+        FILETIME=$(stat $f -c %Y || true)
+
+        if [[ $FILETIME == "" ]]
+        then
+            continue
+        fi
+
+        TIMEDIFF=$(($CURTIME - $FILETIME))
+
+        # Check if file older
+        if [ $TIMEDIFF -gt $OLDTIME ]; then
+            echo "flushing and evicting $f to $base_source"
+            #[ ! -f $f ] ||  &
+
+            if [[ "$flushlist" != "" ]]
+            then
+                flushlist+=" "
             fi
-        done
+
+            flushlist+=$f
+           
+        fi
+        #file_out=$(echo "${f/$s/$base_source}")
+        #for s in "${sources_arr[@]}"
+        #do
+            #if [[ "$f" == "${s}/"* ]]
+            #then
+            #fi
+        #done
     done
-    rsync -a --no-owner --no-group --remove-source-files ${flushlist} ${base_source} || ( sleep 5 && rsync -a --no-owner --no-group --remove-source-files ${flushlist} ${base_source} ) || echo "files cannot be flushed"
+    if [[ ${flushlist} != "" ]]
+    then
+        (mv $flushlist ${base_source} || true) &
+        pid=$!
+        wait $pid
+        #echo "flush and evict ${flushlist}"
+        #rsync -a --no-owner --no-group --remove-source-files ${flushlist} ${base_source} || ( sleep 5 && rsync -a --no-owner --no-group --remove-source-files ${flushlist} ${base_source} ) || echo "files cannot be flushed"
+    fi
 }
 
 flush () {
+        OLDTIME=$1
         re_flush=""
         sources_str=""
         for s in "${sources_arr[@]}"
@@ -61,22 +92,23 @@ flush () {
                     found_files+=" "
                 fi
 
-                found_files+=$(find ${rgx} -type f -follow || echo "")
+                found_files+=$(find ${rgx} -type f -follow 2> /dev/null || true)
             done
             
             if [[ $found_files != "" ]]
             then
-                tobe_flushed=$(ls -tur ${found_files})
-                get_rp_rsync "${tobe_flushed}"
+                tobe_flushed=$(ls -tur ${found_files} 2> /dev/null || true)
+                get_rp_mv "${tobe_flushed}" "$OLDTIME"
             fi
         fi
 }
 
 
 flush_process () {
+    OLDTIME=$1
     while :
     do
-        flush
+        flush $OLDTIME
         sleep 5 &
         wait $!
     done
@@ -84,7 +116,7 @@ flush_process () {
 
 cleanup () {
     echo "Cleaning up Sea mount"
-    time flush
+    time flush 0
 }
 
 
@@ -109,7 +141,7 @@ get_sources () {
 
 }
 
-trap 'cleanup' SIGINT SIGTERM EXIT
+trap 'cleanup' SIGTERM
 get_sources
-flush_process
+flush_process 5
 
