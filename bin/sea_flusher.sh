f6e8ff10cf8bc35afdedfc2d84df9e3313d48de4
valhayot <valeriehayot@gmail.com>
2020-07-22 18:26:13 -0400

    added eviction to in-memory

--- bin/sea_flusher.sh
+++ bin/sea_flusher.sh
@@ -9,15 +9,16 @@ sources_arr=()
 n_sources=0
 conf_file=${SEA_HOME}/sea.ini
 flush_file=${SEA_HOME}/.sea_flushlist
+evict_file=${SEA_HOME}/.sea_evictlist
 tmpfile=$(mktemp /tmp/.sea_flush.inprog.XXXXXX)
 
-mv_old () {
+fe_old () {
 
     found_files=$1 # files that currently exist in source mounts
     OLDTIME=$2 # user specified expiration time
-    task=$3 # the name of the calling task (either "process" or "cleanup")
+    task=$3 # mv / cp / rm (depending on which list)
+    flist="" # list containing files for flush/eviction
 
-    flushlist=""
     for f in $found_files
     do
         
@@ -41,30 +42,45 @@ mv_old () {
 
         # Check if file older
         if [ $TIMEDIFF -ge $OLDTIME ]; then
-            echo "flushing and evicting $f to $base_source"
+            echo "$task $f to $base_source"
 
             # some string formatting
-            if [[ "$flushlist" != "" ]]
+            if [[ "$flist" != "" ]]
             then
-                flushlist+=" "
+                flist+=" "
             fi
 
-            flushlist+=$f
+            flist+=$f
            
         fi
     done
 
-    # if we have some files that older that OLDTIME, move them
-    if [[ ${flushlist} != "" ]]
+    # if we have some files that older that OLDTIME, flush/evict them
+    if [[ ${flist} != "" ]]
     then
 
-        # mv each image individually (as opposed to all at once)
-        for f in $flushlist
-        do
-            ([ ! -f $f ] || mv $f ${base_source} || true) &
-        done
+        # process each image individually (as opposed to all at once)
+        if [[ $task == "mv" ]]
+        then 
+            for f in $flist
+            do
+                ([ ! -f $f ] || mv $f ${base_source} || true) &
+            done
+        elif [[ $task == "cp" ]]
+        then 
+            for f in $flist
+            do
+                ([ ! -f $f ] || cp $f ${base_source} || true) &
+            done
+        elif [[ $task == "rm" ]]
+        then 
+            for f in $flist
+            do
+                ([ ! -f $f ] || rm $f || true) &
+            done
+        fi
 
-        # wait for all images to be moved
+        # wait for all images to be processed
         wait
 
         #(mv $flushlist ${base_source} || true) &
@@ -73,48 +89,172 @@ mv_old () {
     fi
 }
 
+
+
+get_rgx () {
+    SOURCE=$1
+    FILE=$2
+    rgx=""
+
+    if [ -f $FILE ]
+    then
+        while IFS="" read -r re || [ -n "$re" ]
+        do
+            if [[ $rgx != "" ]]
+            then
+                rgx+=" "
+            fi
+
+            rgx+=$SOURCE/$re
+
+        done < $FILE
+
+    fi
+
+    echo $rgx
+}
+
 flush () {
         OLDTIME=$1 # user specified expiration time
         task=$2 # the name of the calling task. either "process" or "cleanup"
 
         re_flush="" # regex of files to flush
+        re_evict="" # regex of files to pin to local disk (don't flush or evict)
+        re_all="" # regex for all files
+        flush_files="" # files to flush only
+        evict_files="" # files to evict only
+        fe_files="" # files to flush and evict
+        all_files="" # all files in source mounts
         sources_str="" # string containing all source paths
 
         # load all potential regex from file (concatenate the regex to existing source mounts)
         for s in "${sources_arr[@]}"
         do
-            while IFS="" read -r re || [ -n "$re" ]
-            do
-                if [[ $re_flush != "" ]]
-                then
-                    re_flush+=" "
-                fi
+            # load rgx of files that need to be flushed
+            if [[ $re_flush != "" ]]
+            then
+                re_flush+=" "
+            fi
+            re_flush+=$(get_rgx $s $flush_file)
 
-                re_flush+=$s/$re
+            # do the same for files that need to be pinned
+            if [[ $re_evict != "" ]]
+            then
+                re_evict+=" "
+            fi
+            re_evict+=$(get_rgx $s $evict_file)
 
-            done < $flush_file
+            if [[ $re_all != "" ]]
+            then
+                re_all+=" "
+            fi
+            re_all+=$s/*
         done
 
-        # if .sea_flushlist file contains regex
-        if [[ $re_flush != "" ]]
-        then
-            found_files=""
-
-            # Look for files that match provided regex patterns
-            for rgx in ${re_flush}
-            do
-                if [[ $found_files != "" ]]
-                then
-                    found_files+=" "
-                fi
-                found_files+=$(find ${rgx} -type f -follow 2> /dev/null || true)
-            done
-            
-            # check to see if there are any candidate files for flushing and mv them
-            if [[ $found_files != "" ]]
+        for rgx in $re_all
+        do
+            if [[ $all_files != "" ]]
             then
-                mv_old "${found_files}" "$OLDTIME" "$task"
+                all_files+=" "
             fi
+            all_files+=$(find ${rgx} -type f -follow 2> /dev/null || true)
+        done
+        echo $all_files
+
+        # if .sea_flushlist file contains regex
+        for rgx in $re_flush
+        do
+            if [[ $flush_files != "" ]]
+            then
+                flush_files+=" "
+            fi
+            echo "rgx $rgx"
+            flush_files+=$(echo $all_files | grep -Eo $rgx || true)
+        done
+
+        # if .sea_evictlist file contains regex
+        for rgx in $re_evict
+        do
+            if [[ $evict_files != "" ]]
+            then
+                evict_files+=" "
+            fi
+            evict_files+=$(echo $all_files | grep -Eo $rgx || true)
+        done
+
+        tmp_flush=""
+        tmp_evict=""
+
+        # find the files that only need to be flushed as well as those that need
+        # to be flushed and evicted
+        for ff in $flush_files
+        do
+            match=0
+            for ef in $evict_files
+            do
+                if [[ $ff == $ef ]]
+                then
+                    match=1
+
+                    if [[ $fe_files != "" ]]
+                    then
+                        fe_files+=" "
+                    fi
+                    fe_files+=$ff
+                    break
+                fi
+            done
+            if [[ $match == 0 ]]
+            then
+                if [[ $tmp_flush != "" ]]
+                then
+                    tmp_flush+=" "
+                fi
+                tmp_flush+=$ff
+            fi
+        done
+
+        # find files that only need to be evicted
+        for ef in $evict_files
+        do
+            match=0
+            for fe in $fe_files
+            do
+                if [[ $ef == $fe ]]
+                then
+                    match=1
+                    break
+                fi
+            done
+            if [[ $match == 0 ]]
+            then
+                if [[ $tmp_evict != "" ]]
+                then
+                    tmp_evict+=" "
+                fi
+                tmp_evict+=$ef
+            fi
+        done
+
+        flush_files=$tmp_flush
+        evict_files=$tmp_evict
+
+        #echo "mv files $fe_files"
+        #echo "cp $flush_files"
+        #echo "rm $evict_files"
+
+        # check to see if there are any candidate files for flush/eviction and process them
+        if [[ $fe_files != "" ]]
+        then
+            fe_old "${fe_files}" "$OLDTIME" "mv"
+        fi
+        if [[ $flush_files != "" ]]
+        then
+            fe_old "${flush_files}" "$OLDTIME" "cp"
+        fi
+        if [[ $evict_files != "" ]]
+        then
+            fe_old "${evict_files}" "$OLDTIME" "rm"
         fi
 }
 
@@ -132,10 +272,9 @@ flush_process () {
 
 cleanup () {
     echo "Cleaning up Sea mount"
-    sp=$(jobs -p)
-    [[ $sp != "" ]] && kill -INT $sp
-    rm $tmpfile
-    ps aux | grep vhs
+    #sp=$(jobs -p)
+    #[[ $sp != "" ]] && kill -INT $sp
+    #ps aux | grep vhs
     time flush 0 "cleanup"
 }
 
@@ -161,6 +300,16 @@ get_sources () {
 
 }
 
-trap '[ -f $tmpfile ] && cleanup' SIGTERM 
-get_sources
-flush_process 5
+#get_sources
+#flush 5 "test func"
+
+trap '[ -f $tmpfile ] && rm $tmpfile' SIGTERM SIGINT EXIT
+
+if [[ $1 == 1 ]]
+then
+    get_sources
+    flush_process 5
+else
+    get_sources
+    cleanup
+fi
