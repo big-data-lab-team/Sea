4cc8f8507356f94bf775d9bb7cd326204861175b
valhayot <valeriehayot@gmail.com>
2020-08-29 14:24:38 -0400

    added modifying sea config

--- experiments/incrementation/parse_json.sh
+++ experiments/incrementation/parse_json.sh
@@ -2,14 +2,68 @@
 
 cond_file=$1
 
+sea_home=/home/vhs/Sea
 out_fldr="/mnt/lustre/vhs/output"
 sea_mount="/mnt/lustre/vhs/mount"
 rdir="./results"
 library="/home/vhs/Sea/build/sea.so"
 executable="python increment.py"
+alldisks=("/disk0/vhs/seatmp" "/disk1/vhs/seatmp" "/disk2/vhs/seatmp" "/disk3/vhs/seatmp" "/disk4/vhs/seatmp" "/disk5/vhs/seatmp")
 
 mkdir -p ${rdir}
 ncond=$(jq ". | length" ${cond_file})
+
+
+format_config () {
+    ndisks=$1
+    nthreads=$2
+    fsize=$3
+    exp_fldr=$4
+    strategy=$5
+    niterations=$6
+
+    conf=${exp_fldr}/sea.ini
+    flushlist=${exp_fldr}/.sea_flushlist
+    evictlist=${exp_fldr}/.sea_evictlist
+
+    disks=""
+
+    for ((x=0;x<ndisks;x++))
+    do
+        if [[ $x -gt 0 ]]
+        then
+            disks+=","
+        fi
+        disks+="${alldisks[$x]}"
+    done
+    echo '# Sea configuration                                                      
+[Sea]                                                                    
+mount_dir = '"${sea_mount}"' ;           
+n_levels = 3 ;                                                          
+source_0 = /dev/shm/seatmp ;           
+source_1 = '"$disks"' ;
+source_2 = '"$out_fldr"' ;
+log_level = 0 ; # 4 crashes tests                                        
+log_file = /home/vhs/passthrough/sea.log ;
+max_fs = '"$fsize"' ;
+n_threads = '"$nthreads"' ;' > $conf
+
+    cp $conf ${sea_home}
+
+    final_it=$(($niterations-1))
+
+    if [[ $strategy == "cp" ]]
+    then
+        echo "*" > $flushlist
+    elif [[ $strategy == "mem" ]]
+    then
+        echo "${final_it}*.nii" > $flushlist
+    fi
+    echo "${final_it}*.nii" > $evictlist
+
+    cp $flushlist $evictlist ${sea_home}
+
+}
 
 
 main () {
@@ -24,6 +78,7 @@ main () {
         nfiles=$(jq ".[$i] .nfiles" ${cond_file})
         in_fldr=$(jq -r ".[$i] .in_fldr" ${cond_file})
         niterations=$(jq ".[$i] .niterations" ${cond_file})
+        fsize=$(jq ".[$i] .fsize" ${cond_file})
         strategy=$(jq -r ".[$i] .strategy" ${cond_file})
 
         echo "Experiment 1 Run 0 conditions
@@ -34,6 +89,7 @@ number of disks: $ndisks
 number of nodes: $nnodes
 number of threads: $nthreads
 number of files: $nfiles
+file size in bytes: $fsize
 number of iterations: ${niterations}"
         exp_fldr="${rdir}/${name}"
         rm -rf $exp_fldr/*
@@ -67,9 +123,9 @@ number of iterations: ${niterations}"
 #SBATCH --account=vhs
 #SBATCH --job-name='"$name"'
 #SBATCH --nodes='"$nnodes"'
-#SBATCH --output='"${exp_fldr}"'slurm-%x-%j.out
+#SBATCH --output='"${exp_fldr}"'/slurm-%x-%j.out
 
-source /home/vhs/Sea/.venv/bin/activate
+source '"${sea_home}"'/.venv/bin/activate
     ' > ${sl_script}
 
         if [[ $strategy == "lustre" ]]
@@ -78,12 +134,12 @@ source /home/vhs/Sea/.venv/bin/activate
 srun -N'"$nnodes"' echo "Clearing cache" && sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
     ' >> ${sl_script}
         else
-            echo 'SEA_HOME=/home/vhs/Sea
+            echo 'SEA_HOME='"${sea_home}"'
 
 srun -N'"$nnodes"' rm -rf /disk0/vhs/seatmp /disk1/vhs/seatmp /disk2/vhs/seatmp /disk3/vhs/seatmp /disk4/vhs/seatmp /disk5/vhs/seatmp /dev/shm/seatmp
 srun -N'"$nnodes"' echo "Clearing cache" && sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
 
-    echo "Creating temp source mount directories"
+echo "Creating temp source mount directories"
 srun -N'"$nnodes"' mkdir /dev/shm/seatmp
 srun -N'"$nnodes"' mkdir /disk0/vhs/seatmp /disk1/vhs/seatmp /disk2/vhs/seatmp /disk3/vhs/seatmp /disk4/vhs/seatmp /disk5/vhs/seatmp' >> ${sl_script}
         fi
@@ -121,7 +177,7 @@ srun -N'"$nnodes"' mkdir /disk0/vhs/seatmp /disk1/vhs/seatmp /disk2/vhs/seatmp /
             fi
 
             node_jobs="${job_dir}/jobs_n${node}.txt"
-            echo "$prefix ${in_fldr}$j ${out_mount}" >> ${node_jobs}
+            echo "$prefix ${in_fldr}$j ${out_mount} ${niterations}" >> ${node_jobs}
 
             parallel_script=${exp_fldr}/n${node}_sea_parallel.sh
 
@@ -157,6 +213,11 @@ srun -N'"${nnodes}"' rm -rf /disk0/vhs/seatmp /disk1/vhs/seatmp /disk2/vhs/seatm
 
         echo "Removing files from output directory"
         rm ${out_fldr}/*;
+
+        if [[ $strategy != "lustre" ]]
+        then
+            format_config $ndisks $nthreads $fsize $exp_fldr $strategy $niterations
+        fi
 
     done
 }
