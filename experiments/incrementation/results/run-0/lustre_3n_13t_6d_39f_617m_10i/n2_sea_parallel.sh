e690b244e11e89f0b4c7042864bcfa98fdf6796c
valhayot <valeriehayot@gmail.com>
2020-09-03 13:32:14 -0400

    added some multinode results with the full bigbrain

---
+++ experiments/incrementation/results/run-0/lustre_3n_13t_6d_39f_617m_10i/n2_sea_parallel.sh
@@ -0,0 +1,1 @@
+parallel --jobs 13 < ./results/run-0/lustre_3n_13t_6d_39f_617m_10i/jobs/jobs_n2.txt
---
+++ experiments/incrementation/results/run-1/sea_mem_3n_13t_6d_39f_617m_10i/n2_sea_parallel.sh
@@ -0,0 +1,1 @@
+parallel --jobs 13 < ./results/run-1/sea_mem_3n_13t_6d_39f_617m_10i/jobs/jobs_n2.txt
