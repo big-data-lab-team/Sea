7cc09c89830de79364116e5af50ad7012bf3cef4
valhayot <valeriehayot@gmail.com>
2020-10-15 13:20:00 -0400

    added the remainder of results/some cleanup

--- experiments/incrementation/results/run-0/lustre_3n_13t_6d_39f_617m_10i/n1_sea_parallel.sh
+++
@@ -1,1 +0,0 @@
-parallel --jobs 13 < ./results/run-0/lustre_3n_13t_6d_39f_617m_10i/jobs/jobs_n1.txt
--- experiments/incrementation/results/run-1/sea_cp_3n_13t_6d_39f_617m_10i/n2_sea_parallel.sh
+++
@@ -1,1 +0,0 @@
-parallel --jobs 13 < ./results/run-1/sea_cp_3n_13t_6d_39f_617m_10i/jobs/jobs_n2.txt
