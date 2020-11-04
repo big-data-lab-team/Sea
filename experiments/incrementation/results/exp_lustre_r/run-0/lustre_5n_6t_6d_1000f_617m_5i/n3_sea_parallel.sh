cf3fc41cf3e9efc117528f18f82f11749ea1c942
valhayot <valeriehayot@gmail.com>
2020-11-04 11:25:02 -0500

    added some lustre 1 iteration and 10 iteration results

---
+++ experiments/incrementation/results/exp_lustre_r/run-0/lustre_5n_6t_6d_1000f_617m_5i/n3_sea_parallel.sh
@@ -0,0 +1,1 @@
+parallel --jobs 5 < ./results/exp_lustre_r/run-0/lustre_5n_6t_6d_1000f_617m_5i/jobs/jobs_n3.txt
---
+++ experiments/incrementation/results/exp_lustre_rw/run-2/lustre_5n_6t_6d_1000f_617m_5i/n2_sea_parallel.sh
@@ -0,0 +1,1 @@
+parallel --jobs 6 < ./results/exp_lustre_rw/run-2/lustre_5n_6t_6d_1000f_617m_5i/jobs/jobs_n2.txt
