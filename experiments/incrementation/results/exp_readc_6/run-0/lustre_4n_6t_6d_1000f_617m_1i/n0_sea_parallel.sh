b9b566d1c2012fabe6675df2ca7261fb6d347f67
valhayot <valeriehayot@gmail.com>
2020-11-10 21:46:41 -0500

    added results for benchmarking reads on fast nodes

---
+++ experiments/incrementation/results/exp_readc_6/run-0/lustre_4n_6t_6d_1000f_617m_1i/n0_sea_parallel.sh
@@ -0,0 +1,1 @@
+parallel --jobs 6 < ./results/exp_readc_6/run-0/lustre_4n_6t_6d_1000f_617m_1i/jobs/jobs_n0.txt
---
+++ experiments/incrementation/results/exp_readc_6/run-0/lustre_5n_6t_6d_1000f_617m_1i/n1_sea_parallel.sh
@@ -0,0 +1,1 @@
+parallel --jobs 6 < ./results/exp_readc_6/run-0/lustre_5n_6t_6d_1000f_617m_1i/jobs/jobs_n1.txt
