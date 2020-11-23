642b2926f05e1521d1134ec8bd5530b00da5aa1b
valhayot <valeriehayot@gmail.com>
2020-11-22 21:07:37 -0500

    added disk results

---
+++ experiments/incrementation/results/exp_disk_lustre/run-0/lustre_4n_6t_1d_1000f_617m_10i/n2_sea_parallel.sh
@@ -0,0 +1,1 @@
+parallel --jobs 6 < ./results/exp_disk_lustre/run-0/lustre_4n_6t_1d_1000f_617m_10i/jobs/jobs_n2.txt
---
+++ experiments/incrementation/results/exp_disk_lustre/run-0/lustre_4n_6t_2d_1000f_617m_10i/sbatch_lustre.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --time=90:55:00
+#SBATCH --account=vhs
+#SBATCH --job-name=lustre_4n_6t_2d_1000f_617m_10i
+#SBATCH --nodes=4
+#SBATCH --nodelist=comp02,comp03,comp04,comp06
+#SBATCH --output=./results/exp_disk_lustre/run-0/lustre_4n_6t_2d_1000f_617m_10i/slurm-%x-%j.out
+
+source /home/vhs/Sea/.venv/bin/activate
+    
+
+srun -N4 ../scripts/clear_client_pc.sh
+    
+start=`date +%s.%N`
+srun -N 1 bash ./results/exp_disk_lustre/run-0/lustre_4n_6t_2d_1000f_617m_10i/n0_sea_parallel.sh &
+srun -N 1 bash ./results/exp_disk_lustre/run-0/lustre_4n_6t_2d_1000f_617m_10i/n1_sea_parallel.sh &
+srun -N 1 bash ./results/exp_disk_lustre/run-0/lustre_4n_6t_2d_1000f_617m_10i/n2_sea_parallel.sh &
+srun -N 1 bash ./results/exp_disk_lustre/run-0/lustre_4n_6t_2d_1000f_617m_10i/n3_sea_parallel.sh &
+wait
+
+end=`date +%s.%N`
+
+runtime=$( echo "$end - $start" | bc -l )
+
+echo "Runtime: $runtime"
