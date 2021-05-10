9aac6140dbf36c036a80754a7a6280d3e3b5a102
valhayot <valeriehayot@gmail.com>
2021-05-10 09:46:53 -0400

    changes to readdir and getcwd

--- src/sea.h
+++ src/sea.h
@@ -3,9 +3,9 @@
 
 // taken from https://stackoverflow.com/questions/54520101/xstat-dynamic-symbol-resolving-error-on-64bit
 #ifndef __x86_64__
-# define _STAT_VER_LINUX    3
+#define _STAT_VER_LINUX 3
 #else
-# define _STAT_VER_LINUX    1
+#define _STAT_VER_LINUX 1
 #endif
 
 #include <limits.h>
@@ -18,19 +18,20 @@ int set_internal();
 int get_internal();
 int unset_internal();
 extern std::set<std::string> sea_files;
-int sea_checkpath(const char* path); 
-int sea_getpath(const char* oldpath, char passpath[PATH_MAX], int masked_path);
-int sea_getpath(const char* oldpath, char passpath[PATH_MAX], int masked_path, int source_id);
+int sea_checkpath(const char *path);
+int sea_getpath(const char *oldpath, char passpath[PATH_MAX], int masked_path);
+int sea_getpath(const char *oldpath, char passpath[PATH_MAX], int masked_path, int source_id);
 void initialize_sea();
 void initialize_sea_if_necessary();
 
-struct SEA_DIR {
-    DIR* dirp;
-    DIR** other_dirp;
+struct SEA_DIR
+{
+    DIR *dirp;
+    DIR **other_dirp;
     char type[7] = {'\0'}; // mostly useless. only used in closedir to ensure struct type is right
-    char** dirnames;
+    char **dirnames;
     int curr_index;
+    int total_dp;
 };
 
 #endif
-
