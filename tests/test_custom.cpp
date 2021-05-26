afaa37e3a2d2e3c6cddfb58ba6b63105cec2b88c
valhayot <valeriehayot@gmail.com>
2021-05-26 11:56:20 -0400

    refactored readdir and added test cases

--- tests/test_custom.cpp
+++ tests/test_custom.cpp
@@ -1,26 +1,28 @@
 #include <gtest/gtest.h>
 #include "../src/passthrough.h"
+#include "../src/functions.h"
 #include "../src/sea.h"
 #include "../src/config.h"
 #include <stdlib.h>
 
 // need to add symlink tests here
-TEST(Passthrough, GetCanonical) {
+TEST(Passthrough, GetCanonical)
+{
+    const char paths[6][PATH_MAX] = {{"mount"}, {"./mount/subdir"}, {"../mount/"}, {"/dev/shm/tmp"}, {"/"}, {NULL}};
 
-    initialize_passthrough_if_necessary();
+    for (int i = 0; i < 6; i++)
+    {
+        const char *path = paths[i];
+        char *actualpath;
+        char *expected = (char *)malloc(sizeof(char *) * PATH_MAX);
 
-    const char paths[6][PATH_MAX] = { {"mount"}, {"./mount/subdir"}, {"../mount/"}, {"/dev/shm/tmp"}, {"/"}, {NULL} };
-
-    for(int i = 0 ; i < 6 ; i++){
-        const char * path = paths[i];
-        char* actualpath;
-        char* expected = (char*) malloc(sizeof(char*) * PATH_MAX);
-    
         actualpath = make_file_name_canonical(path);
         realpath(path, expected);
-        
-        if(path == NULL || path[0] == '\0') {
-            if (actualpath == expected) {
+
+        if (path == NULL || path[0] == '\0')
+        {
+            if (actualpath == expected)
+            {
                 ASSERT_TRUE(true);
             }
         }
@@ -29,17 +31,17 @@ TEST(Passthrough, GetCanonical) {
 
         free(expected);
     }
-
 }
 
-TEST(Passthrough, GetPassCanonical) {
+TEST(Passthrough, GetPassCanonical)
+{
     char path[PATH_MAX];
     char passpath[PATH_MAX];
 
-    char * mount_dir = (char*) malloc(sizeof(char*)*PATH_MAX);
-    char * source_0 = (char*) malloc(sizeof(char*)*PATH_MAX);
-    char * source_1 = (char*) malloc(sizeof(char*)*PATH_MAX);
-    
+    char *mount_dir = (char *)malloc(sizeof(char *) * PATH_MAX);
+    char *source_0 = (char *)malloc(sizeof(char *) * PATH_MAX);
+    char *source_1 = (char *)malloc(sizeof(char *) * PATH_MAX);
+
     realpath("../mount", mount_dir);
     realpath("../source", source_0);
     realpath("/dev/shm/source", source_1);
@@ -47,38 +49,40 @@ TEST(Passthrough, GetPassCanonical) {
     get_pass_canonical(path, passpath, mount_dir, source_0, 0);
 
     ASSERT_STREQ(passpath, source_0);
-    ASSERT_STREQ(path, mount_dir);    
+    ASSERT_STREQ(path, mount_dir);
 
     get_pass_canonical(path, passpath, mount_dir, source_1, 0);
 
     ASSERT_STREQ(passpath, source_1);
-    ASSERT_STREQ(path, mount_dir);    
+    ASSERT_STREQ(path, mount_dir);
 
     get_pass_canonical(path, passpath, mount_dir, source_0, 1);
     ASSERT_STREQ(passpath, mount_dir);
-    ASSERT_STREQ(path, source_0);    
+    ASSERT_STREQ(path, source_0);
 }
 
-TEST(Passthrough, CheckIfSeaPath) {
+TEST(Passthrough, CheckIfSeaPath)
+{
 
-    char oldpaths[4][PATH_MAX] = { "mount/test.txt", "mount/subdir/test.txt", "/dev/shm/test.txt", "/" };
-    char expaths[4][PATH_MAX] = { "source/test.txt", "source/subdir/test.txt", "/dev/shm/test.txt", "/" };
+    char oldpaths[4][PATH_MAX] = {"mount/test.txt", "mount/subdir/test.txt", "/dev/shm/test.txt", "/"};
+    char expaths[4][PATH_MAX] = {"source/test.txt", "source/subdir/test.txt", "/dev/shm/test.txt", "/"};
     char path[PATH_MAX];
 
-    realpath("mount", path);
+    ((funcptr_realpath)libc_realpath)("mount", path);
 
-    for (int i = 0 ; i < 4 ; i++) {
-        char * op = oldpaths[i];
+    for (int i = 0; i < 4; i++)
+    {
+        char *op = oldpaths[i];
         // need to reinitialize each time otherwise may retain tail of previous value
         char passpath[PATH_MAX];
-        realpath("source", passpath);
+        ((funcptr_realpath)libc_realpath)("source", passpath);
 
-        char* actualpath;
+        char *actualpath;
         char expected[PATH_MAX];
         actualpath = make_file_name_canonical(op);
-        realpath(expaths[i], expected);
+        ((funcptr_realpath)libc_realpath)(expaths[i], expected);
         int res = check_if_seapath(path, actualpath, passpath);
-       
+
         if (i < 2)
             ASSERT_EQ(res, 1);
         else
@@ -88,25 +92,26 @@ TEST(Passthrough, CheckIfSeaPath) {
     }
 }
 
-TEST(Passthrough, GetRealPath) {
+TEST(Passthrough, GetRealPath)
+{
     initialize_passthrough_if_necessary();
 
     // test relative mount path
-    const char* oldpath = (const char*) "mount/file.txt"; 
+    const char *oldpath = (const char *)"mount/file.txt";
     char passpath[PATH_MAX];
     pass_getpath(oldpath, passpath, 0);
     char rp[PATH_MAX];
-    realpath("source/file.txt", rp);
+    ((funcptr_realpath)libc_realpath)("source/file.txt", rp);
     ASSERT_STREQ(passpath, rp);
 
     // test masking
-    const char* oldpath_1 = (const char*) "source/file.txt"; 
+    const char *oldpath_1 = (const char *)"source/file.txt";
     pass_getpath(oldpath_1, passpath, 1);
     realpath("mount/file.txt", rp);
     ASSERT_STREQ(passpath, rp);
 
     // test non mount path without masking
-    const char* oldpath_2 = (const char*) "/dev/shm/file.txt"; 
+    const char *oldpath_2 = (const char *)"/dev/shm/file.txt";
     pass_getpath(oldpath_2, passpath, 0);
     strcpy(rp, oldpath_2);
     ASSERT_STREQ(passpath, rp);
@@ -115,15 +120,117 @@ TEST(Passthrough, GetRealPath) {
     pass_getpath(oldpath_2, passpath, 1);
     strcpy(rp, oldpath_2);
     ASSERT_STREQ(passpath, rp);
-
 }
 
+//TODO: Also check error values
+TEST(Sea, OpenDir)
+{
 
-TEST(Sea, InitializeSea) {
+    char *sd = "/subdir";
+
+    // Test if path outside of sea can be opened
+    char *regdir = "newdir";
+
+    DIR *rdp = opendir(regdir);
+    EXPECT_TRUE(rdp != NULL);
+    ((funcptr_closedir)libc_closedir)(rdp);
+
+    // Test if subdir in path outside of Sea can be opened
+    char regsd[PATH_MAX];
+    strcpy(regsd, regdir);
+    strcat(regsd, sd);
+
+    DIR *rsdp = opendir(regsd);
+    EXPECT_TRUE(rsdp != NULL);
+    ((funcptr_closedir)libc_closedir)(rsdp);
+
+    // Test if Sea mount point can be opened
+    char *seadir = "mount";
+    DIR *sdp = opendir(seadir);
+    EXPECT_TRUE(sdp != NULL);
+
+    SEA_DIR *seadp = (SEA_DIR *)sdp;
+
+    if (seadp->dirp == NULL)
+        printf("%s is NULL\n", seadp->dirnames[0]);
+    ((funcptr_closedir)libc_closedir)(seadp->dirp);
+
+    for (int i = 1; i < seadp->total_dp; i++)
+    {
+        ((funcptr_closedir)libc_closedir)(seadp->other_dirp[i - 1]);
+    }
+
+    // Test if subdir in sea can be opened
+    char seasd[PATH_MAX];
+    strcpy(seasd, seadir);
+    strcat(seasd, sd);
+
+    DIR *ssdirp = opendir(seasd);
+    EXPECT_TRUE(sdp != NULL);
+
+    SEA_DIR *seasdp = (SEA_DIR *)ssdirp;
+
+    ((funcptr_closedir)libc_closedir)(seasdp->dirp);
+    for (int i = 1; i < seasdp->total_dp; i++)
+    {
+        ((funcptr_closedir)libc_closedir)(seasdp->other_dirp[i - 1]);
+    }
+
+    free(seadp);
+    free(seasdp);
+
+    // Check that a directory which does not exist fails
+    char *fakedir = "mount/fkdir";
+    DIR *fkdir = opendir(fakedir);
+
+    EXPECT_TRUE(fkdir == NULL);
+}
+
+TEST(Sea, ReadDir)
+{
+    // Test nonseadir
+    char *regdir = "newdir";
+    struct dirent *entry;
+
+    int files = 0;
+    // 32bit
+    DIR *rd = opendir(regdir);
+
+    while ((entry = readdir(rd)))
+    {
+        files++;
+        printf("reading file %s\n", entry->d_name);
+    }
+    ((funcptr_closedir)libc_closedir)(rd);
+
+    ASSERT_EQ(files, 3);
+
+    // 64bit
+    files = 0;
+    rd = opendir(regdir);
+    struct dirent64 *entry64;
+
+    while ((entry64 = readdir64(rd)))
+    {
+        files++;
+        printf("reading64 file %s\n", entry64->d_name);
+    }
+    ((funcptr_closedir)libc_closedir)(rd);
+
+    ASSERT_EQ(files, 3);
+
+    // 64bit
+    files = 0;
+
+    // Test sea mount
+
+    // Test sea subdirectory
+}
+
+TEST(Sea, InitializeSea)
+{
 
     initialize_sea();
-    // 2 mounts * 2 ('.' & '..') + 2 subdirs * 2 mounts * 3 (dirname & '.' & '..') + 5 files
-    ASSERT_EQ(sea_files.size(), 21);
 
     initialize_passthrough_if_necessary();
     config sea_config = get_sea_config();
@@ -132,16 +239,16 @@ TEST(Sea, InitializeSea) {
     strcpy(added_dir, sea_config.source_mounts[1]);
     strcat(added_dir, "/newdir");
 
-    ((funcptr_mkdir)libc_mkdir)(added_dir, 0666);
-    sea_files.clear();
     initialize_sea();
-    // 21 from before + 6 ((dirname, ., ..)*2)
-    ASSERT_EQ(sea_files.size(), 27);
-
-
 }
 
-int main(int argc, char **argv) {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+
+    initialize_passthrough_if_necessary();
+    ((funcptr_mkdir)libc_mkdir)("newdir", 0666);
+    ((funcptr_mkdir)libc_mkdir)("newdir/subdir", 0666);
+
+    return RUN_ALL_TESTS();
 }
