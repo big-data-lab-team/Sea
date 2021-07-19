e8cab65b9b96663ee7beb83a75743acc3fe288d9
valhayot <valeriehayot@gmail.com>
2021-07-19 16:08:10 -0400

    created separate program for source dir mirroring and some fixes to fd functions

--- src/config.h
+++ src/config.h
@@ -6,15 +6,16 @@ typedef struct config
     bool parsed;
     int n_levels;
     int n_sources;
-    char ** source_mounts;
-    char * mount_dir;
-    char * log_file;
+    char **source_mounts;
+    char *mount_dir;
+    char *log_file;
     int log_level;
     unsigned long max_fs;
     int n_threads;
 } config;
 
-char * get_config_file();
+char *get_config_file();
 struct config get_sea_config();
+void parse_config();
 
 #endif
