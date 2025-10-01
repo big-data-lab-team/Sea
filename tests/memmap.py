ca57764763be585a958dcb63274f33cad9039229
valhayot <valeriehayot@gmail.com>
2025-10-01 15:40:18 -0400

    Fix bug in memmap test

--- tests/memmap.py
+++ tests/memmap.py
@@ -5,11 +5,11 @@ import sys
 
 filename = sys.argv[1]
 
-with open(filename, "w+") as f:
-    f.write("Hello World")
+with open(filename, "wb+") as f:
+    f.write(b"Hello World")
 
-with open(filename, "r+") as f:
+with open(filename, "rb+") as f:
     mm = mmap.mmap(f.fileno(), 5)
-    mm[2:4] = "yy"
+    mm[2:4] = b"yy"
     mm.flush()
     mm.close()
